a11741268e88be7cae97aea2275ec592ae4650bc
Siddappa <44073565+Siddapa@users.noreply.github.com>
2020-06-20 21:46:31 -0500

    Part 3 Week 1 Python for Research. Took for 40 min ez

---
+++ PythonForResearch/dna_data.py
@@ -0,0 +1,35 @@
+inputfile = "dna.txt"
+translatefile = 'protein.txt'
+protein = ''
+table = {
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
+}
+
+with open(inputfile, 'r') as f:
+    data = f.read()[20: 938]
+    for i in range(0, len(data), 3):
+        codon = data[i: i+3]
+        if table[codon] == '_':
+            continue
+        protein += table[codon]
+
+with open(translatefile, 'r') as f:
+    if protein == f.read():
+        print(True)
+
+print(protein)
